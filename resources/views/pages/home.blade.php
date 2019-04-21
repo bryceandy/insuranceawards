11b872a82b6d08238c10ba58a16993bcd3d78fe2
bryceandy <bryceandy@rocketmail.com>
2019-04-21 22:34:35 +0300

    Add navigation links

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,13 +6,25 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="{{'css/app.min.css'}}">
+    <link rel="stylesheet" href="{{'css/all.min.css'}}">
 
     <title>Glow - Insurance Awards 2019</title>
 </head>
 <body>
 
+<div id="homeContent">
 
+    <div id="landing">
+        <div class="navigation">
+            <a href="/awards">Awards</a>
+            <a href="/categories">Categories</a>
+            <a href="/about">About</a>
+            <a href="/about">Contact</a>
+        </div>
+
+    </div>
+
+</div>
 
 </body>
 </html>
