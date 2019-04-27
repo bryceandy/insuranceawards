54df821c5eec264ef2b8b279f4726b58d51a225c
bryceandy <bryceandy@rocketmail.com>
2019-04-27 09:42:33 +0300

    Style footer and add additional views

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,9 +16,9 @@
 
     <div id="landingTop">
         <div class="navigation">
-            <a href="#">Awards</a>
+            <a href="/awards">Awards</a>
             <a href="/judges">Judges</a>
-            <a href="#">Forms</a>
+            <a href="/forms">Forms</a>
             <a href="/about">About</a>
             <a href="/contact">Contact</a>
         </div>
@@ -59,15 +59,15 @@
          </span>
          <span class="two">
              Professionals shall constitute the selection committee.The public shall
-             be invited to nominate individuals, or organisations with an outstanding performance .
+             be invited to nominate individuals or firms with an outstanding performance.
          </span>
          <span class="three">
              Finalists for each category will be chosen and required
         to submit supporting documentation for the respective selections.
          </span>
         <span class="four">
-            Awards that need voting, naming etc, a specific media plan shall be developed
-        and will include all media platforms with a dedicated independent website and application
+            For awards that need voting a media plan shall be developed and will include
+         all media platforms with a dedicated independent website and application
         </span>
     </div>
 
@@ -106,6 +106,23 @@
 
     <div id="footer">
 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <path fill="#15ace4" d="M0,95 0,100 100,20 100,15 Z"></path>
+            <path fill="black" d="M0,95 0,80 9,88 Z"></path>
+            <path fill="#ffeb3b" d="M0,80 0,75 14.5,88.35 11.5,91 Z"></path>
+            <path fill="green" d="M0,75 0,10 40,63.2 11.65,85.7 Z" ></path>
+            <path fill="#15ace4" d="M0,10 0,5 42.5,64 41,67 Z"></path>
+        </svg>
+
+        <p>Rewarding Insurance Excellence <br> in Tanzania</p>
+
+        <p>Copyright &copy; Insurance Awards Tanzania 2019</p>
+
+        <div class="footer-links">
+            <div><a href="/awards">Awards</a></div>
+            <div><a href="/forms">Forms</a></div>
+            <div><a href="/contact">Contact</a></div>
+        </div>
 
     </div>
 
