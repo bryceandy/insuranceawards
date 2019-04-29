65a4e96a375595ce626fd245aa9eef656a630ec0
bryceandy <bryceandy@rocketmail.com>
2019-04-29 16:53:52 +0300

    Remove typo

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 set('application', 'Insurance Awards');
 
 // Project repository
-set('repository', 'insurance@github:bryceandy/insuranceawards.git');
+set('repository', 'git@github:bryceandy/insuranceawards.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
