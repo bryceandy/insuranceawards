d601ab96bc410fa8fe0be1cbcae00159d62ea1e2
bryceandy <bryceandy@rocketmail.com>
2019-04-29 16:52:45 +0300

    Change remote git

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 set('application', 'Insurance Awards');
 
 // Project repository
-set('repository', 'insurance@178.128.7.30:insurance/project.git');
+set('repository', 'insurance@github:bryceandy/insuranceawards.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
