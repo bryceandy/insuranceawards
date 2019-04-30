c80d5d673387173c903eb2fc48c877c89c181afc
bryceandy <bryceandy@rocketmail.com>
2019-04-30 16:05:50 +0300

    Input selector

--- resources/views/voting/home.blade.php
+++ resources/views/voting/home.blade.php
@@ -195,7 +195,7 @@
             //display votes
             for (let i = 0; i< names.length; i++){
 
-                console.log($("#icoya input[value='"+names[i]+"'] > input"));
+                console.log($("#icoya input[value='"+names[i]+"'] ").val());
                 $("#icoya input[value='"+names[i]+"'] > input").html("lame"+icoya[i]);
                 // $("#miip input[value='"+names[i]+"'] >").html(""+miip[i]);
                 // $("#mce input[value='"+names[i]+"'] >").html(""+mce[i]);
