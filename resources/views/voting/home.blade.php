74059b1ae7a57b918ec5a314a36d410729fe24d9
bryceandy <bryceandy@rocketmail.com>
2019-04-30 15:20:06 +0300

    Append votes

--- resources/views/voting/home.blade.php
+++ resources/views/voting/home.blade.php
@@ -192,10 +192,10 @@
             let mioya = {!! json_encode($mioya->toArray(), JSON_HEX_TAG) !!};
             let ya = {!! json_encode($ya->toArray(), JSON_HEX_TAG) !!};
 
-            console.log($("#icoya ."+names[0]).length);
+            console.log($("#icoya label ."+names[0]).length);
             console.log($("#icoya ."+names[1]).length);
             console.log($("#icoya ."+names[3]).length);
-            console.log($("#icoya ."+names[4]).length);
+            console.log($("#icoya label ."+names[4]).length);
             //display votes
             for (let i = 0; i< names.length; i++){
 
