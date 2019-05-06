1d7bcc46c73d1239a69d5f08fa814b7aae7f41fd
bryceandy <bryceandy@rocketmail.com>
2019-05-06 21:28:13 +0300

    Judges page

--- resources/views/pages/judges.blade.php
+++ resources/views/pages/judges.blade.php
@@ -8,7 +8,118 @@
 
     <div id="judgesContent">
 
+        <div id="judgesHeader">
+            <h1 class="text-center">2019 Judges</h1>
+        </div>
 
+        <div id="judges">
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+            <div class="judge">
+                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <div class="name text-center">Jane Doe</div>
+                <div class="title text-center">Managing Director</div>
+                <div class="company text-center">Awesome Insurance Company Ltd</div>
+            </div>
+
+        </div>
 
     </div>
 
