724e41f47865d9a5e9aafb5578d87605085f8bd5
bryceandy <bryceandy@rocketmail.com>
2019-05-21 13:02:46 +0300

    Add judges photos

--- resources/views/pages/judges.blade.php
+++ resources/views/pages/judges.blade.php
@@ -15,56 +15,56 @@
         <div id="judges">
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <img src="{{'images/ilomo.png'}}" alt="Judge">
                 <div class="name text-center">Peter Ilomo</div>
                 <div class="title text-center">Former Chairperson and Board Member</div>
                 <div class="company text-center">TIRA</div>
             </div>
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <img src="{{'images/kakusa.png'}}" alt="Judge">
                 <div class="name text-center">Rajab Kakusa</div>
                 <div class="title text-center">JC Vice-Chairperson</div>
                 <div class="company text-center">CEO at TAN-RE</div>
             </div>
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <img src="{{'images/katundu.png'}}" alt="Judge">
                 <div class="name text-center">Dr Jamal Adam Katundu</div>
                 <div class="title text-center">JC Secretary</div>
                 <div class="company text-center">Senior Lecturer and Researcher IFM</div>
             </div>
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <img src="{{'images/mshiu.png'}}" alt="Judge">
                 <div class="name text-center">Octavian Elimeny Mshiu</div>
                 <div class="title text-center">Acting President</div>
                 <div class="company text-center">Tanzania Chamber of Commerce Industries and Agriculture</div>
             </div>
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <img src="{{'images/ulanga.png'}}" alt="Judge">
                 <div class="name text-center">John Ulanga</div>
                 <div class="title text-center">Country Director</div>
                 <div class="company text-center">TradeMark East Africa in Tanzania</div>
             </div>
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
-                <div class="name text-center">Neema Kiure Msusa</div>
+                <img src="{{'images/msusa.png'}}" alt="Judge">
+                <div class="name text-center">Neema Msusa</div>
                 <div class="title text-center">Partner</div>
                 <div class="company text-center">Ernst & Young Tanzania</div>
             </div>
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <img src="{{'images/rughani.png'}}" alt="Judge">
                 <div class="name text-center">Sanjay Rughani</div>
                 <div class="title text-center">CEO</div>
                 <div class="company text-center">Standard Chatered Bank</div>
             </div>
 
             <div class="judge">
-                <img src="{{'images/avatar.png'}}" alt="Judge">
+                <img src="{{'images/accaro.png'}}" alt="Judge">
                 <div class="name text-center">Louis Accaro</div>
                 <div class="title text-center">Director of Membership Services</div>
                 <div class="company text-center">Tanzania Private Sector Foundation</div>
