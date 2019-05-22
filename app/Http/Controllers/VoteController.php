643a190d9cebd1cf4229b7dc8654766531604977
bryceandy <bryceandy@rocketmail.com>
2019-05-22 15:05:48 +0300

    Recover variables in the vote page

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -10,18 +10,17 @@ class VoteController extends Controller
 {
     public function index(){
 
-//        $voteNames = Vote::all()->pluck('name');
-//
-//        $icoya = Vote::all()->pluck('icoya');
-//        $miip = Vote::all()->pluck('miip');
-//        $yaa1 = Vote::all()->pluck('yaa1');
-//        $yaa2 = Vote::all()->pluck('yaa2');
-//        $iboya = Vote::all()->pluck('iboya');
-//        $iacoya = Vote::all()->pluck('iacoya');
-//        $csra = Vote::all()->pluck('csra');
+        $voteNames = Vote::all()->pluck('name');
 
-        return view ('voting.home');
-//        return view('voting.home', compact('voteNames', 'icoya', 'miip', 'yaa1', 'yaa2', 'iboya', 'iacoya', 'csra' ));
+        $icoya = Vote::all()->pluck('icoya');
+        $miip = Vote::all()->pluck('miip');
+        $yaa1 = Vote::all()->pluck('yaa1');
+        $yaa2 = Vote::all()->pluck('yaa2');
+        $iboya = Vote::all()->pluck('iboya');
+        $iacoya = Vote::all()->pluck('iacoya');
+        $csra = Vote::all()->pluck('csra');
+
+        return view('voting.home', compact('voteNames', 'icoya', 'miip', 'yaa1', 'yaa2', 'iboya', 'iacoya', 'csra' ));
     }
 
     /**
