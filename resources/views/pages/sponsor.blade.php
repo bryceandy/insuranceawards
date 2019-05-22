4910fc907456f3c3e2fc8d7af8bd5f16e9cae414
bryceandy <bryceandy@rocketmail.com>
2019-05-22 18:46:02 +0300

    Sponsors page

---
+++ resources/views/pages/sponsor.blade.php
@@ -0,0 +1,22 @@
+@extends('template.default')
+
+@section('title')
+    The Tanzania Insurance Awards 2019 - Award Sponsors
+@endsection
+
+@section('content')
+
+    <div id="sponsorContent">
+
+        <h1 class="text-center">Our Sponsors</h1>
+
+        <div class="sponsors">
+            <img src="{{'images/zepre.png'}}" alt="ZEP-RE">
+            <img src="{{'images/tanre.png'}}" alt="TAN-RE">
+            <img src="{{'images/tira.png'}}" alt="TIRA">
+        </div>
+    </div>
+
+@endsection
+
+
