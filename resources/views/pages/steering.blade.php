4f7e5f3f3db15c8514e0ec9ea416b36dad955e2f
bryceandy <bryceandy@rocketmail.com>
2019-05-22 16:44:03 +0300

    Steering content ans styles

---
+++ resources/views/pages/steering.blade.php
@@ -0,0 +1,79 @@
+@extends('template.default')
+
+@section('title')
+    The Tanzania Insurance Awards 2019 - Steering committee
+@endsection
+
+@section('content')
+
+    <div id="steeringContent">
+
+        <div id="steeringHeader">
+            <h1 class="text-center">Steering Committee</h1>
+        </div>
+
+        <div id="steeringContainer">
+
+            <div class="steering">
+                <img src="{{'images/ilomo.png'}}" alt="Judge">
+                <div class="name text-center">Peter Ilomo</div>
+                <div class="title text-center">Former Chair & Board Member</div>
+                <div class="company text-center">TIRA</div>
+            </div>
+
+            <div class="steering">
+                <img src="{{'images/kakusa.png'}}" alt="Judge">
+                <div class="name text-center">Rajab Kakusa</div>
+                <div class="title text-center">JC Vice-Chairperson</div>
+                <div class="company text-center">CEO at TAN-RE</div>
+            </div>
+
+            <div class="steering">
+                <img src="{{'images/katundu.png'}}" alt="Judge">
+                <div class="name text-center">Dr Jamal Katundu</div>
+                <div class="title text-center">JC Secretary</div>
+                <div class="company text-center">Senior Lecturer and Researcher IFM</div>
+            </div>
+
+            <div class="steering">
+                <img src="{{'images/mshiu.png'}}" alt="Judge">
+                <div class="name text-center">Octavian Mshiu</div>
+                <div class="title text-center">Acting President</div>
+                <div class="company text-center">Tanzania Chamber of Commerce Industries and Agriculture</div>
+            </div>
+
+            <div class="steering">
+                <img src="{{'images/ulanga.png'}}" alt="Judge">
+                <div class="name text-center">John Ulanga</div>
+                <div class="title text-center">Country Director</div>
+                <div class="company text-center">TradeMark East Africa in Tanzania</div>
+            </div>
+
+            <div class="steering">
+                <img src="{{'images/msusa.png'}}" alt="Judge">
+                <div class="name text-center">Neema Msusa</div>
+                <div class="title text-center">Partner</div>
+                <div class="company text-center">Ernst & Young Tanzania</div>
+            </div>
+
+            <div class="steering">
+                <img src="{{'images/rughani.png'}}" alt="Judge">
+                <div class="name text-center">Sanjay Rughani</div>
+                <div class="title text-center">CEO</div>
+                <div class="company text-center">Standard Chatered Bank</div>
+            </div>
+
+            <div class="steering">
+                <img src="{{'images/accaro.png'}}" alt="Judge">
+                <div class="name text-center">Louis Accaro</div>
+                <div class="title text-center">Director of Membership Services</div>
+                <div class="company text-center">Tanzania Private Sector Foundation</div>
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
+
+
