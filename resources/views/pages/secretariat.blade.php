a789286819d20c19d2b07d70b8c045cf857e35d1
bryceandy <bryceandy@rocketmail.com>
2019-05-22 17:08:10 +0300

    Add secretariat styles and container

---
+++ resources/views/pages/secretariat.blade.php
@@ -0,0 +1,58 @@
+@extends('template.default')
+
+@section('title')
+    The Tanzania Insurance Awards 2019 - Secretariat
+@endsection
+
+@section('content')
+
+    <div id="secretariatContent">
+
+        <div id="secretariatHeader">
+            <h1 class="text-center">Secretariat</h1>
+        </div>
+
+        <div id="secretariatContainer">
+
+            <div class="secretariat">
+                <img src="{{'images/saqware.jpeg'}}" alt="Judge">
+                <div class="name text-center">Baghayo Saqware</div>
+                <div class="title text-center">Commissioner</div>
+                <div class="company text-center">TIRA</div>
+            </div>
+
+            <div class="secretariat">
+                <img src="{{'images/bugali.png'}}" alt="Judge">
+                <div class="name text-center">Bosco Bugali</div>
+                <div class="title text-center"></div>
+                <div class="company text-center"></div>
+            </div>
+
+            <div class="secretariat">
+                <img src="{{'images/kilumbi.jpeg'}}" alt="Judge">
+                <div class="name text-center">Ernest Kilumbi</div>
+                <div class="title text-center"></div>
+                <div class="company text-center"></div>
+            </div>
+
+            <div class="secretariat">
+                <img src="{{'images/jaffer.jpeg'}}" alt="Judge">
+                <div class="name text-center">Mohammed Jaffer</div>
+                <div class="title text-center"></div>
+                <div class="company text-center"></div>
+            </div>
+
+            <div class="secretariat">
+                <img src="{{'images/brown.jpeg'}}" alt="Judge">
+                <div class="name text-center">Elizabeth Brown</div>
+                <div class="title text-center"></div>
+                <div class="company text-center"></div>
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
+
+
