2f933d4e860750e4cb3d5515ac0b1c62a5cfda99
bryceandy <bryceandy@rocketmail.com>
2019-05-23 10:36:14 +0300

    About page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,8 +6,49 @@
 
 @section('content')
 
-    <div id="aboutContent">
+    <div id="aboutContent" style="background: #ffffff; padding: 5vw">
 
+        <h1 class="text-center">Annual Insurance Excellence Awards 2019</h1>
+
+        <p>
+            Appreciation is fundamental in human life. Although each one of us is driven entirely by a different set of
+            compulsions e.g. success, fame, honour, self-realisation etc. Human beings respond to appreciation expressed
+            through recognition of good work done. This confirms that their deeds are worthy and valued; as such, satisfaction
+            and  productivity increase to levels never seen before. This is a direct result of the motivation brought
+            about by the reward. Hence, ‘a constant desire to improve’. <br><br>
+
+            The Annual Insurance Excellence Awards shall partake the annual responsibility of Identifying, Recognising
+            and Rewarding outstanding performance in the Insurance Industry in Tanzania.  The gala dinner red carpet event
+            is intended to be Tanzania’s flagship gathering of the Insurance professionals, consultants, providers’,
+            well-wishers, donors, academia, and political leadership, in a bid to create motivation, mass awareness and
+            image boosting in the sector. <br><br>
+
+            The awards ceremony shall be an undertaking by and for all key stakeholders in the Insurance industry in
+            Tanzania and beyond. This is largely because stakeholders have an invaluable role to play when it comes to
+            promoting and sustaining the Insurance business.
+            Being an inaugural gathering planned and commissioned by Tanzania Insurance Regulatory Authority and Insurance
+            Institute of Tanzania, key associations like; Tanzania Insurance Brokers Association, Association of Tanzania
+            Insurers and Loss Assessors and valuers. Tanzania Insurance Awards shall be an extension of the commitment to the
+            advancement of professionalism in the overall insurance industry in Tanzania. <br><br>
+        </p>
+
+        <h4 class="text-left"> Key Objectives</h4> <br>
+        <b>The purpose for the annual insurance awards is to:</b> <br>
+
+        <ul>
+            <li>Identify, Recognise and Reward outstanding players in the insurance industry in Tanzania.</li>
+            <li>To provide knowledge and information by sharing best practice, experiences and expertise.</li>
+            <li>Foster innovation and creativity in the insurance sector.</li>
+            <li>Build awareness and confidence in the public, NGO’s, Donors, the civil society e.t.c.</li>
+            <li>To further promote the role of government in ensuring that insurance is a resource for all and encourage local participation.</li>
+            <li>Enhance greater regional collaboration in the development and promotion of insurance.</li>
+            <li>Accelerate effective use of information, education, communication and technology (IECT) in the insurance sector.</li>
+
+        </ul>
+
+        <p>
+            The fulfilment of the above objectives will further cement the existing relationship between  insurance stakeholders and government, thus simplfying the core activities of providing the much needed insurance services to the patient public. The awards will be proudly and uniquely Tanzanian.
+        </p>
 
     </div>
 
