1205091fe9ec99bdc8996315ac49610683100cf3
bryceandy <bryceandy@rocketmail.com>
2019-05-30 22:21:49 +0300

    Remove crdentials from github

--- config/services.php
+++ config/services.php
@@ -15,9 +15,9 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN', 'insuranceawards.or.tz'),
-        'secret' => env('MAILGUN_SECRET', '03e99a4fb22b2c8764b7b1bd33ee72d3-39bc661a-7a9c7ce5'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.eu.mailgun.net'),
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT'),
     ],
 
     'postmark' => [
