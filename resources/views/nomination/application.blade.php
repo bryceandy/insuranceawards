10c4b326a2bd44007905f374a38ee499fa4c3a36
bryceandy <bryceandy@rocketmail.com>
2019-05-30 10:13:06 +0300

    Init the application page

---
+++ resources/views/nomination/application.blade.php
@@ -0,0 +1,27 @@
+@extends('template.default')
+
+@section('title')
+    The Tanzania Insurance Awards 2019 - Application for Nomination
+@endsection
+
+@section('content')
+
+    <div id="faqsContent" style="padding: 5vw; background: #ffffff">
+
+        <img src="{{'images/comingsoon.png'}}" alt="Comming Soon" style="width:90vw; height: auto; display: block">
+    </div>
+
+@endsection
+
+@section('scripts')
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+
+            $('#nominationLink').addClass('activePage')
+        })
+
+    </script>
+@endsection
+
+
