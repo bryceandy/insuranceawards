29357045a5d6bf5cf8321d9aeb1b5c7e26d13e98
bryceandy <bryceandy@rocketmail.com>
2019-06-02 18:42:03 +0300

    Add Google analytics config

--- resources/views/template/default.blade.php
+++ resources/views/template/default.blade.php
@@ -11,6 +11,16 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     @yield('stylesheets')
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141273597-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-141273597-1');
+    </script>
+
     <title>@yield('title')</title>
 </head>
 <body>
