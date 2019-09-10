6448a2647fe20c689ef65180be1e24b6f2f08a78
bryceandy <bryceandy@rocketmail.com>
2019-09-10 18:07:02 +0300

    Chunk scss files and add shortlist

--- resources/views/pages/shortlist.blade.php
+++ resources/views/pages/shortlist.blade.php
@@ -6,9 +6,27 @@
 
 @section('content')
 
-    <div id="shortlistContent" style="padding: 5vw; background: #ffffff">
+    <div id="shortlistContent">
 
-        <img src="{{'images/comingsoon.png'}}" alt="Comming Soon" style="width:90vw; height: auto; display: block">
+        <h1 class="text-center">Shortlisted Nominees</h1>
+
+        <h3>Insurance Company of the Year</h3>
+
+        <ul>
+            <li>Sanlam Life Insurance Tanzania</li>
+            <li>Alliance Insurance Corporation Limited</li>
+            <li>Alliance Life Assurance Limited</li>
+            <li>Jubilee Insurance Limited Company</li>
+        </ul>
+
+        <h3>Most Innovative Insurance Product</h3>
+
+        <ul>
+            <li>Jubilee Insurance Limited Company - Safari Voucher</li>
+            <li>Milvik Tanzania Limited - Bima Mkononi</li>
+            <li>Mgen Tanzania Insurance Company Limited - Multi-Peril Crop Insurance</li>
+            <li>Acclavia Insurance Brokers - Boda Salama</li>
+        </ul>
     </div>
 
 @endsection
