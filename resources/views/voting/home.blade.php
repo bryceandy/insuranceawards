212a4887909615a9aa4fe969000b543ff71e380f
bryceandy <bryceandy@rocketmail.com>
2019-09-10 09:32:13 +0300

    Remove unused form fields

--- resources/views/voting/home.blade.php
+++ resources/views/voting/home.blade.php
@@ -90,165 +90,165 @@
 
         </form>
 
-        <form action="/vote" method="post" id="yaa1" >
-            @csrf
+        {{--<form action="/vote" method="post" id="yaa1" >--}}
+            {{--@csrf--}}
 
-            <label for="award">
-                Young Achievers' Award - Ideas/Research Development
-                <input type="hidden" name="award" value="yaa1" >
-            </label> <br>
+            {{--<label for="award">--}}
+                {{--Young Achievers' Award - Ideas/Research Development--}}
+                {{--<input type="hidden" name="award" value="yaa1" >--}}
+            {{--</label> <br>--}}
 
-            <label for="Choice1">
-                <input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">
-                Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>
-            </label>
+            {{--<label for="Choice1">--}}
+                {{--<input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">--}}
+                {{--Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice2">
-                <input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">
-                Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>
-            </label>
+            {{--<label for="Choice2">--}}
+                {{--<input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">--}}
+                {{--Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice3">
-                <input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">
-                Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>
-            </label>
+            {{--<label for="Choice3">--}}
+                {{--<input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">--}}
+                {{--Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice4">
-                <input type="radio" id="Choice4" name="name" value="I.I.T">
-                I.I.T <span class="I.I.T"></span>
-            </label>
+            {{--<label for="Choice4">--}}
+                {{--<input type="radio" id="Choice4" name="name" value="I.I.T">--}}
+                {{--I.I.T <span class="I.I.T"></span>--}}
+            {{--</label>--}}
 
-            <button type="submit" name="submit3">Vote</button>
+            {{--<button type="submit" name="submit3">Vote</button>--}}
 
-        </form>
+        {{--</form>--}}
 
-        <form action="/vote" method="post" id="yaa2" >
-            @csrf
+        {{--<form action="/vote" method="post" id="yaa2" >--}}
+            {{--@csrf--}}
 
-            <label for="award">
-                Young Achievers' Award - Entrepreneurship
-                <input type="hidden" name="award" value="yaa2" >
-            </label> <br>
+            {{--<label for="award">--}}
+                {{--Young Achievers' Award - Entrepreneurship--}}
+                {{--<input type="hidden" name="award" value="yaa2" >--}}
+            {{--</label> <br>--}}
 
-            <label for="Choice1">
-                <input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">
-                Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>
-            </label>
+            {{--<label for="Choice1">--}}
+                {{--<input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">--}}
+                {{--Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice2">
-                <input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">
-                Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>
-            </label>
+            {{--<label for="Choice2">--}}
+                {{--<input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">--}}
+                {{--Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice3">
-                <input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">
-                Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>
-            </label>
+            {{--<label for="Choice3">--}}
+                {{--<input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">--}}
+                {{--Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice4">
-                <input type="radio" id="Choice4" name="name" value="I.I.T">
-                I.I.T <span class="I.I.T"></span>
-            </label>
+            {{--<label for="Choice4">--}}
+                {{--<input type="radio" id="Choice4" name="name" value="I.I.T">--}}
+                {{--I.I.T <span class="I.I.T"></span>--}}
+            {{--</label>--}}
 
-            <button type="submit" name="submit4">Vote</button>
+            {{--<button type="submit" name="submit4">Vote</button>--}}
 
-        </form>
+        {{--</form>--}}
 
-        <form action="/vote" method="post" id="iboya" >
-            @csrf
+        {{--<form action="/vote" method="post" id="iboya" >--}}
+            {{--@csrf--}}
 
-            <label for="award">
-                Insurance Brokerage of the Year
-                <input type="hidden" name="award" value="iboya" >
-            </label><br>
+            {{--<label for="award">--}}
+                {{--Insurance Brokerage of the Year--}}
+                {{--<input type="hidden" name="award" value="iboya" >--}}
+            {{--</label><br>--}}
 
-            <label for="Choice1">
-                <input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">
-                Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>
-            </label>
+            {{--<label for="Choice1">--}}
+                {{--<input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">--}}
+                {{--Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice2">
-                <input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">
-                Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>
-            </label>
+            {{--<label for="Choice2">--}}
+                {{--<input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">--}}
+                {{--Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice3">
-                <input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">
-                Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>
-            </label>
+            {{--<label for="Choice3">--}}
+                {{--<input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">--}}
+                {{--Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice4">
-                <input type="radio" id="Choice4" name="name" value="I.I.T">
-                I.I.T <span class="I.I.T"></span>
-            </label>
+            {{--<label for="Choice4">--}}
+                {{--<input type="radio" id="Choice4" name="name" value="I.I.T">--}}
+                {{--I.I.T <span class="I.I.T"></span>--}}
+            {{--</label>--}}
 
-            <button type="submit" name="submit5">Vote</button>
+            {{--<button type="submit" name="submit5">Vote</button>--}}
 
-        </form>
+        {{--</form>--}}
 
-        <form action="/vote" method="post" id="iacoya" >
-            @csrf
+        {{--<form action="/vote" method="post" id="iacoya" >--}}
+            {{--@csrf--}}
 
-            <label for="award">
-                Insurance Awareness Campaign of the Year
-                <input type="hidden" name="award" value="iacoya" >
-            </label><br>
+            {{--<label for="award">--}}
+                {{--Insurance Awareness Campaign of the Year--}}
+                {{--<input type="hidden" name="award" value="iacoya" >--}}
+            {{--</label><br>--}}
 
-            <label for="Choice1">
-                <input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">
-                Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>
-            </label>
+            {{--<label for="Choice1">--}}
+                {{--<input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">--}}
+                {{--Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice2">
-                <input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">
-                Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>
-            </label>
+            {{--<label for="Choice2">--}}
+                {{--<input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">--}}
+                {{--Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice3">
-                <input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">
-                Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>
-            </label>
+            {{--<label for="Choice3">--}}
+                {{--<input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">--}}
+                {{--Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice4">
-                <input type="radio" id="Choice4" name="name" value="I.I.T">
-                I.I.T <span class="I.I.T"></span>
-            </label>
+            {{--<label for="Choice4">--}}
+                {{--<input type="radio" id="Choice4" name="name" value="I.I.T">--}}
+                {{--I.I.T <span class="I.I.T"></span>--}}
+            {{--</label>--}}
 
-            <button type="submit" name="submit6">Vote</button>
+            {{--<button type="submit" name="submit6">Vote</button>--}}
 
-        </form>
+        {{--</form>--}}
 
-        <form action="/vote" method="post" id="csra" >
-            @csrf
+        {{--<form action="/vote" method="post" id="csra" >--}}
+            {{--@csrf--}}
 
-            <label for="award">
-                Corporate Social Responsibility Award
-                <input type="hidden" name="award" value="csra" >
-            </label><br>
+            {{--<label for="award">--}}
+                {{--Corporate Social Responsibility Award--}}
+                {{--<input type="hidden" name="award" value="csra" >--}}
+            {{--</label><br>--}}
 
-            <label for="Choice1">
-                <input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">
-                Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>
-            </label>
+            {{--<label for="Choice1">--}}
+                {{--<input type="radio" id="Choice1" name="name" value="Tanzania Insurance Brokers Association">--}}
+                {{--Tanzania Insurance Brokers Association <span class="Tanzania Insurance Brokers Association"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice2">
-                <input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">
-                Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>
-            </label>
+            {{--<label for="Choice2">--}}
+                {{--<input type="radio" id="Choice2" name="name" value="Tanzania Insurance Regulatory Authority">--}}
+                {{--Tanzania Insurance Regulatory Authority <span class="Tanzania Insurance Regulatory Authority"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice3">
-                <input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">
-                Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>
-            </label>
+            {{--<label for="Choice3">--}}
+                {{--<input type="radio" id="Choice3" name="name" value="Association of Tanzania Insurers">--}}
+                {{--Association of Tanzania Insurers <span class="Association of Tanzania Insurers"></span>--}}
+            {{--</label>--}}
 
-            <label for="Choice4">
-                <input type="radio" id="Choice4" name="name" value="I.I.T">
-                I.I.T <span class="I.I.T"></span>
-            </label>
+            {{--<label for="Choice4">--}}
+                {{--<input type="radio" id="Choice4" name="name" value="I.I.T">--}}
+                {{--I.I.T <span class="I.I.T"></span>--}}
+            {{--</label>--}}
 
-            <button type="submit" name="submit7">Vote</button>
+            {{--<button type="submit" name="submit7">Vote</button>--}}
 
-        </form>
+        {{--</form>--}}
 
     </div>
 
@@ -263,22 +263,22 @@
             let names = {!! json_encode($voteNames->toArray(), JSON_HEX_TAG) !!};
             let icoya = {!! json_encode($icoya->toArray(), JSON_HEX_TAG) !!};
             let miip = {!! json_encode($miip->toArray(), JSON_HEX_TAG) !!};
-            let yaa1 = {!! json_encode($yaa1->toArray(), JSON_HEX_TAG) !!};
-            let yaa2 = {!! json_encode($yaa2->toArray(), JSON_HEX_TAG) !!};
-            let iboya = {!! json_encode($iboya->toArray(), JSON_HEX_TAG) !!};
-            let iacoya = {!! json_encode($iacoya->toArray(), JSON_HEX_TAG) !!};
-            let csra = {!! json_encode($csra->toArray(), JSON_HEX_TAG) !!};
+            {{--let yaa1 = {!! json_encode($yaa1->toArray(), JSON_HEX_TAG) !!};--}}
+            {{--let yaa2 = {!! json_encode($yaa2->toArray(), JSON_HEX_TAG) !!};--}}
+            {{--let iboya = {!! json_encode($iboya->toArray(), JSON_HEX_TAG) !!};--}}
+            {{--let iacoya = {!! json_encode($iacoya->toArray(), JSON_HEX_TAG) !!};--}}
+            {{--let csra = {!! json_encode($csra->toArray(), JSON_HEX_TAG) !!};--}}
 
             //display votes
             for (let i = 0; i< names.length; i++){
 
                 $("#icoya span[class='"+names[i]+"'] ").html(icoya[i]);
-                $("#iacoya span[class='"+names[i]+"'] ").html(iacoya[i]);
-                $("#csra span[class='"+names[i]+"'] ").html(csra[i]);
+                // $("#iacoya span[class='"+names[i]+"'] ").html(iacoya[i]);
+                // $("#csra span[class='"+names[i]+"'] ").html(csra[i]);
                 $("#miip span[class='"+names[i]+"'] ").html(miip[i]);
-                $("#yaa2 span[class='"+names[i]+"'] ").html(yaa2[i]);
-                $("#iboya span[class='"+names[i]+"'] ").html(iboya[i]);
-                $("#yaa1 span[class='"+names[i]+"'] ").html(yaa1[i]);
+                // $("#yaa2 span[class='"+names[i]+"'] ").html(yaa2[i]);
+                // $("#iboya span[class='"+names[i]+"'] ").html(iboya[i]);
+                // $("#yaa1 span[class='"+names[i]+"'] ").html(yaa1[i]);
 
             }
 
